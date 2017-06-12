e7e3984a950cca0f978e7357bba360d54e85d207
Michael Angerman <stormasm@gmail.com>
2017-06-12 15:49:38 -0700

    initial commit

---
+++ src/server/main.js
@@ -0,0 +1,31 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
+
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import mySchema from '../schema';
+
+const app = express();
+
+// Requests to /graphql redirect to /
+app.all('/graphql', (req, res) => res.redirect('/'));
+
+app.use(
+  '/',
+  graphqlHTTP(() => ({
+    schema: mySchema,
+    graphiql: true,
+  })),
+);
+
+// Listen for incoming HTTP requests
+
+const listener = app.listen(3000);
+const port = listener.address().port;
+/* eslint-disable no-console */
+console.log('Listening on port ' + port);
