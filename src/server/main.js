aea34cc66e24ac7ed459060e810877e4c1cd9a14
Michael Angerman <stormasm@gmail.com>
2017-06-13 08:06:08 -0700

    blogSchema and data

--- src/server/main.js
+++ src/server/main.js
@@ -3,7 +3,7 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import {
     mySchema
-} from '../schema/starWarsSchema';
+} from '../schema/blogSchema';
 
 const app = express();
 
