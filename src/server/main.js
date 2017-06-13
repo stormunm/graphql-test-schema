ca478ed490dd5f8546a5a7974c5ddb0d8b93138a
Michael Angerman <stormasm@gmail.com>
2017-06-13 07:42:19 -0700

    cc

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,3 @@
-/**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE-examples file in the root directory of this source tree.
- */
 
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
